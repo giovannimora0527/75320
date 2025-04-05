d4de29b85444c0bd6cfa73c4645612ad6121e29d
DanielPerez <daniel.sperez1903@gmail.com>
2025-04-04 19:39:44 -0500

    Tarea 2do corte

---
+++ 75320-front-desarrollo/75320-front-desarrollo/src/app/libros/libros.module.ts
@@ -0,0 +1,11 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { LibrosComponent } from './libros.component';
+
+@NgModule({
+  imports: [
+    CommonModule
+  ],
+  declarations: [LibrosComponent]
+})
+export class LibrosModule { }
